0cf7e82f94262a46bd8ebdee7f1e88755f15ab85
devjindev <hyjinny00@gmail.com>
2022-05-05 02:51:36 +0900

    🔗 redux: user

--- src/containers/common/HeaderForm.tsx
+++ src/containers/common/HeaderForm.tsx
@@ -1,26 +1,19 @@
 import React from "react";
 import Header from "../../components/common/Header";
 import { useNavigate } from "react-router";
+import { RootStateOrAny, useSelector } from "react-redux";
 
 const HeaderForm = () => {
   const navigate = useNavigate();
   const onLogout = () => {
     console.log("로그아웃");
-    localStorage.removeItem("tm-token");
     navigate("/");
   };
 
-  return (
-    <Header
-      user={{
-        nickname: "닉네임예시",
-        email: "email1234@google.com",
-        password: "pw1234",
-      }}
-      // user={null}
-      onLogout={onLogout}
-    />
-  );
+  const { user } = useSelector((state: RootStateOrAny) => state.user);
+  console.log("user: ", user);
+
+  return <Header user={user} onLogout={onLogout} />;
 };
 
 export default HeaderForm;
