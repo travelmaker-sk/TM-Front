223f63f74845d569a2385401f9add06803b288bb
devjindev <hyjinny00@gmail.com>
2022-05-06 18:38:15 +0900

    push

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,20 +3,21 @@ import Responsive from "../components/common/Responsive";
 import styled from "styled-components";
 import HeaderForm from "../containers/common/HeaderForm";
 
-export const Wrapeer = styled(Responsive)``;
+// export const Wrapeer = styled(Responsive)``;
+export const Wrapeer = styled.div``;
 
 export const MarginBottom = styled.div`
   // Desktop
   @media screen and (min-width: 1280px) {
-    height: 130px;
+    height: 80px;
   }
   // Tablet
   @media screen and (min-width: 768px) and (max-width: 1279px) {
-    height: 130px;
+    height: 80px;
   }
   // Mobile
   @media screen and (max-width: 767px) {
-    height: 100px;
+    height: 65px;
   }
 `;
 
@@ -26,7 +27,6 @@ const PostListPage = () => {
       <HeaderForm />
       <MarginBottom />
       <img src="./images/bg1.png" alt="bg" />
-      <img src="./images/bg2.png" alt="bg" />
     </Wrapeer>
   );
 };
