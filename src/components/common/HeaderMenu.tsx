b5c4e1a707308e045f26909ee8bd3cca593f3aec
devjindev <hyjinny00@gmail.com>
2022-06-11 15:36:04 +0900

    header menu

--- src/components/common/HeaderMenu.tsx
+++ src/components/common/HeaderMenu.tsx
@@ -19,7 +19,6 @@ const HeaderMenuStyle = styled.div`
     user-select: none;
   }
   nav ul {
-    display: none;
     position: absolute;
     top: 150%;
     right: 0;
@@ -29,9 +28,6 @@ const HeaderMenuStyle = styled.div`
     font-family: "Noto Sans KR", sans-serif;
     box-shadow: 5px 5px 10px 0 rgba(0, 0, 0, 0.2);
     border-radius: 10px;
-  }
-  nav ul.open {
-    display: block;
   }
   nav ul li {
     margin-bottom: 20px;
@@ -83,37 +79,41 @@ const HeaderMenu = ({ onLogout }: HeaderMenuProps) => {
     <HeaderMenuStyle>
       <nav className="material-icons apps-bar" onClick={onToggle}>
         apps
-        <ul className={isToggle ? "open" : ""}>
-          <li>
-            <Link to="/mypage">
-              <span className="material-icons">account_circle</span>
-              마이페이지
-            </Link>
-          </li>
-          <li>
-            <Link to="/createPhotocard">
-              <span className="material-icons">add_photo_alternate</span>
-              포토카드 만들기
-            </Link>
-          </li>
-          <li>
-            <Link to="/bookmarks">
-              <span className="material-icons">bookmarks</span>
-              북마크
-            </Link>
-          </li>
-          <li>
-            <Link to="/setProfile">
-              <span className="material-icons">manage_accounts</span>
-              회원정보 설정
-            </Link>
-          </li>
-          <hr />
-          <button onClick={onLogout}>
-            <span className="material-icons">logout</span>
-            로그아웃
-          </button>
-        </ul>
+        {isToggle ? (
+          <ul>
+            <li>
+              <Link to="/mypage">
+                <span className="material-icons">account_circle</span>
+                마이페이지
+              </Link>
+            </li>
+            <li>
+              <Link to="/createPhotocard">
+                <span className="material-icons">add_photo_alternate</span>
+                포토카드 만들기
+              </Link>
+            </li>
+            <li>
+              <Link to="/bookmarks">
+                <span className="material-icons">bookmarks</span>
+                북마크
+              </Link>
+            </li>
+            <li>
+              <Link to="/setProfile">
+                <span className="material-icons">manage_accounts</span>
+                회원정보 설정
+              </Link>
+            </li>
+            <hr />
+            <button onClick={onLogout}>
+              <span className="material-icons">logout</span>
+              로그아웃
+            </button>
+          </ul>
+        ) : (
+          ""
+        )}
       </nav>
     </HeaderMenuStyle>
   );
